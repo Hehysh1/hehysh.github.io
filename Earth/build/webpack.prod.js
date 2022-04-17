da57bba60a199fa0317dc0397b8b3d833c7ff456
hehysh <hehysh@163.com>
2022-04-17 19:05:39 +0800

    Site updated: 2022-04-17 19:05:39

--- Earth/build/webpack.prod.js
+++
@@ -1,9 +0,0 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="/assets/css/APlayer.min.css"><script src="/assets/js/APlayer.min.js" class="aplayer-secondary-script-marker"></script>const commonConfig = require("./webpack.common")
-const merge = require("webpack-merge");
-
-const prodConfig = {
-    mode: "production",
-    devtool: "cheap-module-eval-source-map",
-}
-
-module.exports = merge(commonConfig, prodConfig)
